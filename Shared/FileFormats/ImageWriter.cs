5f65c9d0853ad225b0cc9561ad8191010523110d
Pieter Witvoet <p.witvoet@gmail.com>
2026-05-04 17:50:34 +0200

    Applied @tonysergi's fix for model portrait color remapping: indexed bitmaps now have their colors-used field explicitly set to the number of colors, because Half-Life doesn't seem to interpret the special value 0 correctly.

--- Shared/FileFormats/ImageWriter.cs
+++ Shared/FileFormats/ImageWriter.cs
@@ -21,12 +21,24 @@ namespace Shared.FileFormats
 
         public void WriteIndexedImage(IndexedImage indexedImage, string path, ImageFormat format)
         {
-            // NOTE: The custom 'quantizer' contains the actual image data and palette, but we still need an Image instance so we can use its Save method:
-            using (var indexedImageSavingQuantizer = new IndexedImageSavingQuantizer(indexedImage))
-            using (var dummyImage = new Image<Rgba32>(indexedImage.Width, indexedImage.Height))
+            if (format == ImageFormat.Bmp)
             {
-                var imageEncoder = GetIndexedImageEncoder(format, indexedImageSavingQuantizer);
-                dummyImage.Save(path, imageEncoder);
+                using (var memoryStream = new MemoryStream())
+                {
+                    WriteIndexedImage(indexedImage, memoryStream, format);
+                    SetBitmapColorsUsedField(memoryStream, indexedImage.Palette.Length);
+
+                    using (var file = File.Create(path))
+                    {
+                        memoryStream.Position = 0;
+                        memoryStream.CopyTo(file);
+                    }
+                }
+            }
+            else
+            {
+                using (var file = File.Create(path))
+                    WriteIndexedImage(indexedImage, file, format);
             }
         }
 
@@ -41,6 +53,17 @@ namespace Shared.FileFormats
                 case ImageFormat.Gif: return new GifEncoder();
                 case ImageFormat.Bmp: return new BmpEncoder();
                 case ImageFormat.Tga: return new TgaEncoder { BitsPerPixel = TgaBitsPerPixel.Pixel32 };
+            }
+        }
+
+        private static void WriteIndexedImage(IndexedImage indexedImage, Stream stream, ImageFormat format)
+        {
+            // NOTE: The custom 'quantizer' contains the actual image data and palette, but we still need an Image instance so we can use its Save method:
+            using (var indexedImageSavingQuantizer = new IndexedImageSavingQuantizer(indexedImage))
+            using (var dummyImage = new Image<Rgba32>(indexedImage.Width, indexedImage.Height))
+            {
+                var imageEncoder = GetIndexedImageEncoder(format, indexedImageSavingQuantizer);
+                dummyImage.Save(stream, imageEncoder);
             }
         }
 
@@ -71,6 +94,36 @@ namespace Shared.FileFormats
                     };
 
                 default: throw new NotSupportedException($"{format} format does not support indexed images.");
+            }
+        }
+
+
+        /// <summary>
+        /// This sets the biClrUsed field of the bitmap info header to the number of colors used.
+        /// ImageSharp sets that field to 0, which means that the maximum number of colors for the given bit-depth is used,
+        /// but somehow Half-Life doesn't interpret that value correctly because it breaks color remapping for multiplayer model portraits.
+        /// </summary>
+        private static void SetBitmapColorsUsedField(Stream stream, int colorsCount)
+        {
+            stream.Position = 0;
+            var fileSignature = stream.ReadString(2);
+            if (fileSignature != "BM")
+                return;
+
+
+            stream.Position = 46;
+            var colorsUsed = stream.ReadInt();
+
+            if (colorsUsed == 0)
+            {
+                stream.Position = 28;
+                var bitsPerPixel = stream.ReadUint();
+                var maxColors = 1 << (int)bitsPerPixel;
+
+                colorsUsed = Math.Clamp(colorsCount, 1, maxColors);
+
+                stream.Position = 46;
+                stream.Write(colorsUsed);
             }
         }
     }
